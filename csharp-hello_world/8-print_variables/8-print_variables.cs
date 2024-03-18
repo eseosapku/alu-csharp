78e15369cb189357cf03b9a25b45e0e261b535a7
eseosapku <75255976+eseosapku@users.noreply.github.com>
2024-03-18 13:25:31 +0200

    co

--- csharp-hello_world/8-print_variables/8-print_variables.cs
+++ csharp-hello_world/8-print_variables/8-print_variables.cs
@@ -7,6 +7,6 @@ class Program
         string str1 = "Street";
         string str2 = "Mission";
         int int1 = 972;
-	Console.WriteLine("{0} {1} {2}", str1, str2, int1);
+	Console.WriteLine("{0} {1} {2}", int1, str2, str1);
     }
 }
--- csharp-hello_world/8-print_variables/obj/Debug/net7.0/8-print_variables.AssemblyInfo.cs
+++ csharp-hello_world/8-print_variables/obj/Debug/net7.0/8-print_variables.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("8-print_variables")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ccd0a1db5f1e6c2a312217407dbae4aef96fec2a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f1487c31874b804d66af9da51e138d5a241ed08")]
 [assembly: System.Reflection.AssemblyProductAttribute("8-print_variables")]
 [assembly: System.Reflection.AssemblyTitleAttribute("8-print_variables")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
