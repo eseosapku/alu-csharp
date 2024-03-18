cc3ca61dbc0bfb033ff171637df9bc7c4491aebc
eseosapku <75255976+eseosapku@users.noreply.github.com>
2024-03-18 12:49:06 +0200

    comit

--- csharp-hello_world/6-print_string/obj/Debug/net7.0/6-print_string.AssemblyInfo.cs
+++ csharp-hello_world/6-print_string/obj/Debug/net7.0/6-print_string.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("6-print_string")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f28670e4d41e02ead4f3ea7cf256ffc71543b95")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e9fc0757e4b3915a085661d6284cbafc9911d8ca")]
 [assembly: System.Reflection.AssemblyProductAttribute("6-print_string")]
 [assembly: System.Reflection.AssemblyTitleAttribute("6-print_string")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
