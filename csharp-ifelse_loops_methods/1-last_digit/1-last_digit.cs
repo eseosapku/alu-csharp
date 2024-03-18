b78760dfa9feb022c8f9a142df282a84d5c94f05
eseosapku <75255976+eseosapku@users.noreply.github.com>
2024-03-18 15:21:32 +0200

    comit

---
+++ csharp-ifelse_loops_methods/1-last_digit/1-last_digit.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+Random rndm = new Random();
+        int number = rndm.Next(-10000, 10000);
+        int lastDigit = Math.Abs(number) % 10;
+        Console.Write("The last digit of {0} is", number);
+        if (lastDigit > 5)
+        {
+            Console.WriteLine("{0} and is greater than 5", lastDigit);
+        }
+        else if (lastDigit == 0)
+        {
+            Console.WriteLine("{0} and is 0", lastDigit);
+        }
+        else
+        {
+            Console.WriteLine("{0} and is less than 6 and not 0", lastDigit);
+        }
