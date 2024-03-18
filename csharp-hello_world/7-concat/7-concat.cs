83c112f0679c8456703465c154e03ba9b85f93ad
eseosapku <75255976+eseosapku@users.noreply.github.com>
2024-03-18 12:55:09 +0200

    comit

--- csharp-hello_world/7-concat/7-concat.cs
+++ csharp-hello_world/7-concat/7-concat.cs
@@ -1,4 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-string str1 = "Holberton";
-string str2 = "School";
-Console.WriteLine("Welcome to {0} {1}!", str1, str2);
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string str1 = "Holberton";
+        string str2 = "School";
+        Console.WriteLine("Welcome to {0} {1}!", str1, str2);
+    }
+}
