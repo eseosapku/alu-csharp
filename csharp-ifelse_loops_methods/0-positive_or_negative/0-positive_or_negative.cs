4d1c428f8cf9908c51a78b64678ccdf9066c47b6
eseosapku <75255976+eseosapku@users.noreply.github.com>
2024-03-18 15:17:09 +0200

    comit

--- csharp-ifelse_loops_methods/0-positive_or_negative/0-positive_or_negative.cs
+++ csharp-ifelse_loops_methods/0-positive_or_negative/0-positive_or_negative.cs
@@ -1,7 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Random rndm = new Random();
         int number = rndm.Next(-10, 10);
-        Console.Write("The number {0} ", number);
+        Console.Write("{0} ", number);
         if (number > 0)
         {
             Console.WriteLine("is positive");
