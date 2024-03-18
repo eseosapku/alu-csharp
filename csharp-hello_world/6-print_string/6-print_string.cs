e9fc0757e4b3915a085661d6284cbafc9911d8ca
eseosapku <75255976+eseosapku@users.noreply.github.com>
2024-03-18 12:43:33 +0200

    comit

--- csharp-hello_world/6-print_string/6-print_string.cs
+++ csharp-hello_world/6-print_string/6-print_string.cs
@@ -1,4 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more informatiion
 string str = "Holberton School";
-Console.WriteLine(str + str + str);
-Console.WriteLine(str.Substring(0, 9));
+Console.WriteLine(str + str + str + "\n" + str.Substring(0, 9));
