d74b398a840fa4002d679ce4b1084d9c7f7c889a
eseosapku <75255976+eseosapku@users.noreply.github.com>
2024-03-18 11:10:36 +0200

    comit

--- csharp-hello_world/4-print_integer/4-print_integer.cs
+++ csharp-hello_world/4-print_integer/4-print_integer.cs
@@ -1,11 +1,10 @@
 ﻿using System;
-
 class Program
 {
     static void Main(string[] args)
     {
         int number = 972;
         Console.WriteLine($"{number}, Mission Street");
-        Console.WriteLine(); 
+        Console.WriteLine(); // followed by a new line
     }
 }
