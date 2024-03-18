cc06eec6e42c5eb87185ac2a41fe651c2382c08e
eseosapku <75255976+eseosapku@users.noreply.github.com>
2024-03-18 12:02:27 +0200

    comit

--- csharp-hello_world/3-writeline/3-writeline.cs
+++ csharp-hello_world/3-writeline/3-writeline.cs
@@ -4,7 +4,6 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("\"Programming is like building a multilingualpuzzle");
-        Console.WriteLine(); 
+        Console.WriteLine("\"Programming is like building a multilingual puzzle\n"); 
     }
 }
--- csharp-hello_world/3-writeline/obj/Debug/net7.0/3-writeline.AssemblyInfo.cs
+++ csharp-hello_world/3-writeline/obj/Debug/net7.0/3-writeline.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("3-writeline")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f9816b9e35fa76ada9c18e731a36b922f681e28")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f5dd13ff148953e301e997b6e9903ef01be18ad6")]
 [assembly: System.Reflection.AssemblyProductAttribute("3-writeline")]
 [assembly: System.Reflection.AssemblyTitleAttribute("3-writeline")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
