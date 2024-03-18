1ce91460b35c63ed0f3a05e07279874499f95cd8
eseosapku <75255976+eseosapku@users.noreply.github.com>
2024-03-18 16:06:06 +0200

    comit

---
+++ csharp-ifelse_loops_methods/4-print_hexa/4-print_hexa.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+{
+	for (int i = 0; i < 99; i++)
+        {
+            Console.Write($"{i} = 0x{i.ToString("X")}\n");
+        }
+    }
