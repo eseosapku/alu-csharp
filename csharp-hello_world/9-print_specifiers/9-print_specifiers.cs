5a7bb8352e8951227e368c5e572e85cd387b6ba0
eseosapku <75255976+eseosapku@users.noreply.github.com>
2024-03-18 13:31:55 +0200

    xoit

---
+++ csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+double percent = .7553;
+double currency = 98765.4321;
+Console.WriteLine("Percent: {0:P2} tip", percent);
+Console.WriteLine("Currency: {0:C}", currency);
