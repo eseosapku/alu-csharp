ccd0a1db5f1e6c2a312217407dbae4aef96fec2a
eseosapku <75255976+eseosapku@users.noreply.github.com>
2024-03-18 13:08:27 +0200

    comit

---
+++ csharp-hello_world/8-print_variables/8-print_variables.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string str1 = "972";
+        string str2 = "Mission Street";
+        Console.WriteLine("{0} {1}", str1, str2);
+    }
+}
