12e0acda8ad03dbe60d718295761cd0bde4f821e
eseosapku <75255976+eseosapku@users.noreply.github.com>
2024-03-19 15:13:51 +0200

    comit

---
+++ csharp-ifelse_loops_methods/9-add/9-add.cs
@@ -0,0 +1,27 @@
+﻿public class Line
+{
+    public static void PrintLine(int length)
+    {
+        // Check if length is 0 or less
+        if (length <= 0)
+        {
+            Console.WriteLine(); // Print only a new line
+        }
+        else
+        {
+            for (int i = 0; i < length; i++)
+            {
+                Console.Write("_"); // Print "_" for the specified length
+            }
+            Console.WriteLine(); // End the line with a new line
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        // Example usage
+        PrintLine(5);
+        PrintLine(0);
+        PrintLine(-3);
+    }
+}
