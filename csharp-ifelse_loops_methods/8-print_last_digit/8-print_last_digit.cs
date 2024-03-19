6de6d3a163e177fedb85c8a46af9f155e2b5e974
eseosapku <75255976+eseosapku@users.noreply.github.com>
2024-03-19 15:07:33 +0200

    comit

---
+++ csharp-ifelse_loops_methods/8-print_last_digit/8-print_last_digit.cs
@@ -0,0 +1,18 @@
+﻿public class Number
+{
+    public static int PrintLastDigit(int number)
+    {
+        int lastDigit = Math.Abs(number % 10);
+        Console.WriteLine("Last digit of {0} is {1}", number, lastDigit);
+        return lastDigit;
+    }
+
+    static void Main(string[] args)
+    {
+        int num1 = 12345;
+        int num2 = -9876;
+        PrintLastDigit(num1);
+        PrintLastDigit(num2);
+    }
+}
+
