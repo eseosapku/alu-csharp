a8075aceec8294d66c158857a21416420948caf5
eseosapku <75255976+eseosapku@users.noreply.github.com>
2024-03-19 14:57:31 +0200

    commit

---
+++ csharp-ifelse_loops_methods/7-islower/7-islower.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+public class Character
+{
+    public static bool IsLower(char c)
+    {
+        return c >= 'a' && c <= 'z';
+    }
+
+    static void Main(string[] args)
+    {
+        // Example usage
+        char ch1 = 'a';
+        char ch2 = 'B';
+        Console.WriteLine($"{ch1} is lowercase: {IsLower(ch1)}");
+        Console.WriteLine($"{ch2} is lowercase: {IsLower(ch2)}");
+    }
+}
+
