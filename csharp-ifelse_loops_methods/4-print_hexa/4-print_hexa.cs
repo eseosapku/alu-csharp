3faf2dcdf831bec5b1412a746546d1da2a0830b9
eseosapku <75255976+eseosapku@users.noreply.github.com>
2024-03-19 14:17:22 +0200

    comit

--- csharp-ifelse_loops_methods/4-print_hexa/4-print_hexa.cs
+++ csharp-ifelse_loops_methods/4-print_hexa/4-print_hexa.cs
@@ -3,7 +3,7 @@
 class Program{
     static void Main(String[] args){
          for(int i = 0; i < 99 ; i++){
-            Console.WriteLine($"{i} = 0x{i:X}");
+            Console.Write($"{i} = 0x{i:x}");
          }
     }
 }
--- csharp-ifelse_loops_methods/4-print_hexa/obj/Debug/net7.0/4-print_hexa.AssemblyInfo.cs
+++ csharp-ifelse_loops_methods/4-print_hexa/obj/Debug/net7.0/4-print_hexa.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("4-print_hexa")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ce91460b35c63ed0f3a05e07279874499f95cd8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+35ced91f727881e8dd1d2c10f8e3a917db0b6b41")]
 [assembly: System.Reflection.AssemblyProductAttribute("4-print_hexa")]
 [assembly: System.Reflection.AssemblyTitleAttribute("4-print_hexa")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
