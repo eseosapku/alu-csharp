4cfc657565195e8dbaf9e77722f2bfba78f6485d
eseosapku <75255976+eseosapku@users.noreply.github.com>
2024-03-19 14:25:14 +0200

    comit

---
+++ csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        for (int i = 0; i < 100; i++)
+        {
+            Console.Write($"{i:D2}");
+            if (i != 99)
+            {
+                Console.Write(", ");
+            }
+            else
+            {
+                Console.WriteLine();
+            }
+        }
+    }
+}
