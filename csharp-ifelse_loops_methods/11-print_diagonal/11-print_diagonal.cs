50643590480a1d0b6c7f89b12f39af75bd09cd00
eseosapku <75255976+eseosapku@users.noreply.github.com>
2024-03-19 15:27:09 +0200

    comit

---
+++ csharp-ifelse_loops_methods/11-print_diagonal/11-print_diagonal.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+public class Line
+{
+    public static void PrintDiagonal(int length)
+    {
+        if (length <= 0)
+        {
+            Console.WriteLine(); 
+        }
+        else
+        {
+            for (int i = 0; i < length; i++)
+            {
+                Console.Write(new string(' ', i)); 
+                Console.WriteLine("\\"); 
+            }
+        }
+    }
+    static void Main(string[] args)
+    {
+        PrintDiagonal(5);
+        PrintDiagonal(0);
+        PrintDiagonal(-3);
+    }
+}
