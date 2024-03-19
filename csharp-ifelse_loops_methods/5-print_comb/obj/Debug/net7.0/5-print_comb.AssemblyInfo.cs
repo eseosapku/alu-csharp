93cdce51336da7c636ad45bf0bd66926f43b354b
eseosapku <75255976+eseosapku@users.noreply.github.com>
2024-03-19 14:35:15 +0200

    comit

--- csharp-ifelse_loops_methods/5-print_comb/obj/Debug/net7.0/5-print_comb.AssemblyInfo.cs
+++ csharp-ifelse_loops_methods/5-print_comb/obj/Debug/net7.0/5-print_comb.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("5-print_comb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4cfc657565195e8dbaf9e77722f2bfba78f6485d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+119bb1ddebb72fdabe2b98f30207e1079ec5d020")]
 [assembly: System.Reflection.AssemblyProductAttribute("5-print_comb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("5-print_comb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
