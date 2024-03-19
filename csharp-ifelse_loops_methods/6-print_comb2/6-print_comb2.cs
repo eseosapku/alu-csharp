ad47fb489990e38bbb826df4ef9df12d2a17f2ee
eseosapku <75255976+eseosapku@users.noreply.github.com>
2024-03-19 14:53:13 +0200

    comit

---
+++ csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+int count = 0; // Variable to count how many combinations are printed
+
+        for (int i = 0; i <= 9; i++)
+        {
+            for (int j = i + 1; j <= 9; j++)
+            {
+                Console.Write($"{i}{j:D}");
+                count++;
+
+                if (count < 45) // Check if it's not the last combination
+                {
+                    Console.Write(", ");
+                }
+                else
+                {
+                    Console.WriteLine(); // Print new line after the last combination
+                }
+            }
+        }
