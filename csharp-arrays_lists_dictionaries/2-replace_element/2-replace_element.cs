432e10ac4d159f63a8acd20423082d929e74e5e7
eseosapku <75255976+eseosapku@users.noreply.github.com>
2024-03-21 11:39:08 +0200

    comit

--- csharp-arrays_lists_dictionaries/2-replace_element/2-replace_element.cs
+++ csharp-arrays_lists_dictionaries/2-replace_element/2-replace_element.cs
@@ -1,16 +1,15 @@
-﻿using System;
+﻿using System.Collections;
 
 class Array{
     public static int[] ReplaceElement(int[] array, int index, int n){
-
-        if (index < 0 ||index > array.Length){
-                Console.WriteLine ("Index Out Of Range");
-                return array;
-            }
-            else{
-                array[index] = n;
-                return array;
-                
-            }
+        int[] cacheArray = array;
+        int size = cacheArray.Length;
+        if(index >= 0 && index < size){
+            cacheArray[index] = n;
+            return cacheArray;
+        }else{
+            Console.WriteLine("Index out of range");
+            return cacheArray;
+        }
     }
 }
