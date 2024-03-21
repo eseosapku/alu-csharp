3c0b15d094d149765493fed55cfa22b076bebb16
eseosapku <75255976+eseosapku@users.noreply.github.com>
2024-03-21 12:11:09 +0200

    co

--- csharp-arrays_lists_dictionaries/3-print_array_reverse/obj/Debug/net7.0/3-print_array_reverse.AssemblyInfo.cs
+++ csharp-arrays_lists_dictionaries/3-print_array_reverse/obj/Debug/net7.0/3-print_array_reverse.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("3-print_array_reverse")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f65f104e4cfa8c2bd55276ddf85a6286e93022a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc0b4b0fe77f0f6f6e6cdd3db063cb000adcdb73")]
 [assembly: System.Reflection.AssemblyProductAttribute("3-print_array_reverse")]
 [assembly: System.Reflection.AssemblyTitleAttribute("3-print_array_reverse")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
