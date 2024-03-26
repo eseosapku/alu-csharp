42cd41e5465bad2aff6a72c902ce9004f55b9c68
eseosapku <75255976+eseosapku@users.noreply.github.com>
2024-03-26 12:37:58 +0200

    cop

--- csharp-arrays_lists_dictionaries/4-print_list/4-print_list.cs
+++ csharp-arrays_lists_dictionaries/4-print_list/4-print_list.cs
@@ -1,19 +1,60 @@
 ﻿using System;
 using System.Collections.Generic;
 
-class List {
-    public static List<int> CreatePrint(int size){
+class List
+{
+    public static List<int> CreatePrint(int size)
+    {
+        if (size <= 0)
+        {
+            Console.WriteLine("Size cannot be zero or negative. Returning an empty list.");
+            return new List<int>();
+        }
+
         List<int> myList = new List<int>();
 
-        if (size == 0 || size < 0){
-            Console.WriteLine("Size cannot be negative");
-            return null;
+        for (int i = 0; i < size; i++)
+        {
+            myList.Add(i);
         }
-        else{
-            for (int i = 0; i = size; i ++){
-                myList.Add(i);
-                return myList;
+
+        return myList;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<int> myList;
+
+        myList = List.CreatePrint(10);
+        PrintList(myList);
+
+        myList = List.CreatePrint(16);
+        PrintList(myList);
+
+        myList = List.CreatePrint(0);
+        PrintList(myList);
+
+        myList = List.CreatePrint(1);
+        PrintList(myList);
+    }
+
+    static void PrintList(List<int> list)
+    {
+        if (list != null)
+        {
+            Console.Write("List: ");
+            foreach (int item in list)
+            {
+                Console.Write(item + " ");
             }
+            Console.WriteLine();
+        }
+        else
+        {
+            Console.WriteLine("List is null");
         }
     }
 }
--- csharp-arrays_lists_dictionaries/4-print_list/obj/Debug/net7.0/4-print_list.AssemblyInfo.cs
+++ csharp-arrays_lists_dictionaries/4-print_list/obj/Debug/net7.0/4-print_list.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("4-print_list")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c3c0b5eadeda986ed7534865ec9f0ebcedcc0f47")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5deb10103eb7debbafb3e961f9904db20049729a")]
 [assembly: System.Reflection.AssemblyProductAttribute("4-print_list")]
 [assembly: System.Reflection.AssemblyTitleAttribute("4-print_list")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
