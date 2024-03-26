0f25604b8b5b461de80879161b464dc2da4a4164
eseosapku <75255976+eseosapku@users.noreply.github.com>
2024-03-26 14:40:11 +0200

    ckk

--- csharp-arrays_lists_dictionaries/3-print_array_reverse/3-print_array_reverse.cs
+++ csharp-arrays_lists_dictionaries/3-print_array_reverse/3-print_array_reverse.cs
@@ -1,24 +1,21 @@
-﻿using System.Collections;
+﻿using System;
 
-class Array{
-
-    public static void Reverse(int[] array){
-     
-       if(array != null){
+class Array {
+    public static void Reverse(int[] array) {
+        if (array != null) {
             int size = array.Length;
-        if(size > 0 && size != null){
-
-            for(int i = size - 1; i >= 0; i--){
-                Console.Write(array[i] + " ");
+            if (size > 0) {
+                for (int i = size - 1; i >= 0; i--) {
+                    Console.Write(array[i]);
+                    if (i != 0) // Add space if it's not the first number
+                        Console.Write(" ");
                 }
-                    Console.WriteLine();
-             }else{
                 Console.WriteLine();
-             }
-       }else{
-        Console.WriteLine();
-       }
-       
-     
+            } else {
+                Console.WriteLine();
+            }
+        } else {
+            Console.WriteLine();
+        }
     }
 }
