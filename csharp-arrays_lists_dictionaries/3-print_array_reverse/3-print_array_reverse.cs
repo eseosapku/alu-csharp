cf7a095feb710e4525c5db3bc0301e1372a9f436
eseosapku <75255976+eseosapku@users.noreply.github.com>
2024-03-26 12:13:46 +0200

    c

--- csharp-arrays_lists_dictionaries/3-print_array_reverse/3-print_array_reverse.cs
+++ csharp-arrays_lists_dictionaries/3-print_array_reverse/3-print_array_reverse.cs
@@ -1,32 +1,16 @@
 ﻿using System;
 
-class Program
-{
-    static void Main(string[] args)
-    {
-        int[] array1 = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-        int[] array2 = null;
-        int[] array3 = {};
-        int[] array4 = {98, -10, 0, 972, -42};
+class Array {
+    public static void Reverse(int[] array){
 
-        PrintArrayReverse(array1);
-        PrintArrayReverse(array2);
-        PrintArrayReverse(array3);
-        PrintArrayReverse(array4);
-    }
-
-    static void PrintArrayReverse(int[] array)
-    {
-        if (array == null || array.Length == 0)
-        {
-            Console.WriteLine(); // Print an empty line for null or empty array
-            return;
+        if (array == null || array == {}){
+            Console.WriteLine("");
         }
-
-        for (int i = array.Length - 1; i >= 0; i--)
-        {
-            Console.Write(array[i] + " ");
+        else{
+            for (int i = array.Length; i == 0; i--){
+            int arrayValue = array[i]; 
+            Console.Write($"{i} ");
         }
-        Console.WriteLine(); // Print a new line after printing the array
+        }
     }
 }
