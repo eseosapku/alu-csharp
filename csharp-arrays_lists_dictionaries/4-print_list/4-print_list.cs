e21a6f3e38dcae6655beaa9f8d3ea6e1232bfeb2
eseosapku <75255976+eseosapku@users.noreply.github.com>
2024-03-26 12:28:34 +0200

    p

--- csharp-arrays_lists_dictionaries/4-print_list/4-print_list.cs
+++ csharp-arrays_lists_dictionaries/4-print_list/4-print_list.cs
@@ -5,40 +5,42 @@ class ListUtils
 {
     public static List<int>? CreatePrint(int size)
     {
-        if (size < 0)
-        {
-            Console.WriteLine("Size cannot be negative");
-            return null;
-        }
-
-        List<int> newList = new List<int>();
-
-        for (int i = 0; i < size; i++)
-        {
-            newList.Add(i);
-            Console.Write(i + " ");
-        }
-
-        Console.WriteLine(); // Print new line after printing the list
-
-        return newList;
+        // Your implementation here
     }
+}
 
-    // Example usage in the Main method (to test the CreatePrint method)
+class Program
+{
     static void Main(string[] args)
     {
-        int size = 10;
-        List<int>? newList = CreatePrint(size);
+        List<int>? newList;
 
-        // Example usage of the created list
+        newList = ListUtils.CreatePrint(10);
         if (newList != null)
         {
-            Console.WriteLine("Created List:");
-            foreach (var item in newList)
-            {
-                Console.Write(item + " ");
-            }
-            Console.WriteLine(); // Print new line after printing the list
+            Console.WriteLine("List Length: " + newList.Count);
+            Console.WriteLine("----------------");
         }
+        
+        newList = ListUtils.CreatePrint(16);
+        if (newList != null)
+        {
+            Console.WriteLine("List Length: " + newList.Count);
+            Console.WriteLine("----------------");
+        }
+        
+        newList = ListUtils.CreatePrint(0);
+        if (newList != null)
+        {
+            Console.WriteLine("List Length: " + newList.Count);
+            Console.WriteLine("----------------");
+        }
+        
+        newList = ListUtils.CreatePrint(1);
+        if (newList != null)
+        {
+            Console.WriteLine("List Length: " + newList.Count);
+            Console.WriteLine("----------------");
+        }        
     }
 }
--- csharp-arrays_lists_dictionaries/4-print_list/obj/Debug/net7.0/4-print_list.AssemblyInfo.cs
+++ csharp-arrays_lists_dictionaries/4-print_list/obj/Debug/net7.0/4-print_list.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("4-print_list")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e47b93e2e621da939702e5ee123be0e70f141546")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+802cd8dd4ac0b74f733ada412b3c4f1a3dc5f34c")]
 [assembly: System.Reflection.AssemblyProductAttribute("4-print_list")]
 [assembly: System.Reflection.AssemblyTitleAttribute("4-print_list")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
