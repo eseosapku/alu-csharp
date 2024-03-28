9fe9b0aa8fd2164452af28bf1afb31d0f0136ee3
eseosapku <75255976+eseosapku@users.noreply.github.com>
2024-03-28 12:13:13 +0200

    somi

---
+++ csharp-arrays_lists_dictionaries/6-divisible_by_2/6-divisible_by_2.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class List
+{
+    public static List<bool> DivisibleBy2(List<int> myList)
+    {
+        List<bool> result = new List<bool>();
+
+        foreach (int num in myList)
+        {
+            result.Add(num % 2 == 0);
+        }
+
+        return result;
+    }
+}
