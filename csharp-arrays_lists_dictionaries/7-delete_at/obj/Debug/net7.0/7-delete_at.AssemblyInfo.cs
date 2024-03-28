8199fc86bed2233e65214d62fd531570319552be
eseosapku <75255976+eseosapku@users.noreply.github.com>
2024-03-28 13:34:05 +0200

    dd

--- csharp-arrays_lists_dictionaries/7-delete_at/obj/Debug/net7.0/7-delete_at.AssemblyInfo.cs
+++ csharp-arrays_lists_dictionaries/7-delete_at/obj/Debug/net7.0/7-delete_at.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("7-delete_at")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c3bf74ea993ff8f1e04b917d4caa407611dae4ba")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+11d74f3eba4fdcab3c2c46bfe6402ad8de44cc8b")]
 [assembly: System.Reflection.AssemblyProductAttribute("7-delete_at")]
 [assembly: System.Reflection.AssemblyTitleAttribute("7-delete_at")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
