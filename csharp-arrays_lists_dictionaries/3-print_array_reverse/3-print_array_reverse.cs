1d1057acc485fd32139aa609cb0c9215085aae71
eseosapku <75255976+eseosapku@users.noreply.github.com>
2024-03-28 11:26:52 +0200

    l

--- csharp-arrays_lists_dictionaries/3-print_array_reverse/3-print_array_reverse.cs
+++ csharp-arrays_lists_dictionaries/3-print_array_reverse/3-print_array_reverse.cs
@@ -1,16 +1,37 @@
-﻿using System;
+﻿using System.Collections;
 
-class Array {
+class Array{
+
+
+    static void Main(string[] args)
+    {
+        int[] array1 = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+        int[] array2 = null;
+        int[] array3 = {};
+        int[] array4 = {98, -10, 0, 972, -42};
+
+        Array.Reverse(array1);
+        Array.Reverse(array2);
+        Array.Reverse(array3);
+        Array.Reverse(array4);
+    }
+
     public static void Reverse(int[] array){
 
-        if (array == null || array == {}){
-            Console.WriteLine("");
-        }
-        else{
-            for (int i = array.Length; i == 0; i--){
-            int arrayValue = array[i]; 
-            Console.Write($"{i} ");
-        }
-        }
+       if(array != null){
+            int size = array.Length;
+        if(size > 0 && size != null){
+            for(int i = size - 1; i >= 0; i--){
+                Console.Write(array[i] + " ");
+                }
+                    Console.WriteLine();
+             }else{
+                Console.WriteLine();
+             }
+       }else{
+        Console.WriteLine();
+       }
+       
+     
     }
 }
