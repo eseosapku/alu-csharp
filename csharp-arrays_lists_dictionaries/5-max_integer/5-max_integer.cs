00de06aa7fe32881b430b80f005545dbdefa566a
eseosapku <75255976+eseosapku@users.noreply.github.com>
2024-03-28 11:44:47 +0200

    pooo

--- csharp-arrays_lists_dictionaries/5-max_integer/5-max_integer.cs
+++ csharp-arrays_lists_dictionaries/5-max_integer/5-max_integer.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+
+public class List
+{
+    public static int MaxInteger(List<int> myList)
+    {
+        if (myList.Count == 0)
+        {
+            Console.WriteLine("List is empty");
+            return -1;
+        }
+
+        int max = myList[0];
+        foreach (int num in myList)
+        {
+            if (num > max)
+            {
+                max = num;
+            }
+        }
+
+        return max;
+    }
+}
