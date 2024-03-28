d7895fab599b3d5e5ae2268cd1732689d6b8b67b
eseosapku <75255976+eseosapku@users.noreply.github.com>
2024-03-28 11:02:35 +0200

    pl

--- csharp-arrays_lists_dictionaries/3-print_array_reverse/3-print_array_reverse.cs
+++ csharp-arrays_lists_dictionaries/3-print_array_reverse/3-print_array_reverse.cs
@@ -1,19 +1,34 @@
 ﻿using System;
 
-class Array {
-    public static void Reversed(int[] array){ 
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] array1 = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+        int[] array2 = null;
+        int[] array3 = {};
+        int[] array4 = {98, -10, 0, 972, -42};
 
-        if (array == null)
+        ReverseArray(array1);
+        ReverseArray(array2); // This will produce a warning due to array2 being null
+        ReverseArray(array3);
+        ReverseArray(array4);
+    }
+
+    static void ReverseArray(int[] array)
+    {
+        if (array != null)
+        {
+            Array.Reverse(array);
+            foreach (int num in array)
+            {
+                Console.Write(num + " ");
+            }
+            Console.WriteLine();
+        }
+        else
         {
             Console.WriteLine("Array is null");
-            return;
         }
-
-        Array.Reverse(array); // Reverse the non-null array
-        foreach (int num in array)
-        {
-            Console.Write($"{num} ");
-        }
-        Console.WriteLine(); // Ensure output ends with a newline for correct formatting
     }
 }
