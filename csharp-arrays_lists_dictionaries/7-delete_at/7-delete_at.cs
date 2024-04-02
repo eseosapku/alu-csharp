ce1fdfb4188d0040de9fe7b48a762a75d40db589
eseosapku <75255976+eseosapku@users.noreply.github.com>
2024-04-02 14:40:09 +0200

    jjk

--- csharp-arrays_lists_dictionaries/7-delete_at/7-delete_at.cs
+++ csharp-arrays_lists_dictionaries/7-delete_at/7-delete_at.cs
@@ -1,24 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 
-class Program
-{
-    static void Main(string[] args)
-    {
-        List<int> myList = new List<int>() { 1, 2, 3, 4, 5 };
-
-        foreach (int i in myList)
-            Console.WriteLine(i);
-
-        Console.WriteLine("----------");
-
-        myList = List.DeleteAt(myList, 2); // Assign the modified list back to myList
-
-        foreach (int i in myList)
-            Console.WriteLine(i);
-    }
-}
-
 public class List
 {
     public static List<int> DeleteAt(List<int> myList, int index)
