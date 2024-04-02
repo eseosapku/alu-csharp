5c864ffd4bc703be649d3e0beb742120e39e1ea6
eseosapku <75255976+eseosapku@users.noreply.github.com>
2024-04-02 17:08:03 +0200

    p

---
+++ csharp-arrays_lists_dictionaries/9-add_key_value/9-add_key_value.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Dictionary
+{
+    public static Dictionary<string, string> AddKeyValue(Dictionary<string, string> myDict, string key, string value)
+    {
+        // Check if the key already exists
+        if (myDict.ContainsKey(key))
+        {
+            // Update the value if the key exists
+            myDict[key] = value;
+        }
+        else
+        {
+            // Add the key-value pair if the key doesn't exist
+            myDict.Add(key, value);
+        }
+
+        return myDict;
+    }
+}
