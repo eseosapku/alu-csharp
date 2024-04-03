b89b4317c006dbbabec71b0f3606549ca6f125eb
eseosapku <75255976+eseosapku@users.noreply.github.com>
2024-04-03 19:50:12 +0200

    jjj

--- csharp-hashset_stack_queue_linkedlist/8-linkedlist_find/8-linkedlist_find.cs
+++ csharp-hashset_stack_queue_linkedlist/8-linkedlist_find/8-linkedlist_find.cs
@@ -3,15 +3,15 @@ using System.Collections.Generic;
 
 class LList
 {
-    public static int FindNode(LinkedList<int> myLList, int value)
+    public static int FindNode(LinkedList<int>? myLList, int value)
     {
-        if (myLList == null || myLList.First == null)
+        if (myLList?.First == null)
         {
             // If the linked list is null or empty, return -1
             return -1;
         }
 
-        LinkedListNode<int> current = myLList.First;
+        LinkedListNode<int>? current = myLList.First;
 
         int index = 0;
         while (current != null)
