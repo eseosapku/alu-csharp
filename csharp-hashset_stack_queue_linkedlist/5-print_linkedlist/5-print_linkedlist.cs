0e7431e4805946fee573061ff1218d9474c527d5
eseosapku <75255976+eseosapku@users.noreply.github.com>
2024-04-03 14:49:12 +0200

    pol

--- csharp-hashset_stack_queue_linkedlist/5-print_linkedlist/5-print_linkedlist.cs
+++ csharp-hashset_stack_queue_linkedlist/5-print_linkedlist/5-print_linkedlist.cs
@@ -1,1 +1,39 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static LinkedList<int> CreatePrint(int size)
+    {
+        LinkedList<int> newList = new LinkedList<int>();
+
+        if (size <= 0)
+        {
+            return newList; // Return an empty list for negative or zero size
+        }
+
+        // Add elements to the list and print them
+        for (int i = 0; i < size; i++)
+        {
+            newList.AddLast(i);
+            Console.WriteLine(i);
+        }
+
+        return newList;
+    }
+}
+
+class Program
+{
+    // Main - entry point
+    static void Main(string[] args)
+    {
+        LinkedList<int> llist;
+        int size = 8;
+
+        llist = LList.CreatePrint(size);
+
+        Console.WriteLine("-------------");
+        Console.WriteLine("Linked List Length: " + llist.Count);
+    }
+}
