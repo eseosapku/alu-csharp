eeca7e668ed34e52e9b28c2a510cd23fcf5cc870
eseosapku <75255976+eseosapku@users.noreply.github.com>
2024-04-03 20:11:41 +0200

    ddds

--- csharp-hashset_stack_queue_linkedlist/13-linkedlist_delete/13-linkedlist_delete.cs
+++ csharp-hashset_stack_queue_linkedlist/13-linkedlist_delete/13-linkedlist_delete.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static void Delete(LinkedList<int> myLList, int index)
+    {
+        if (myLList.First == null || index < 0)
+        {
+            return; // No elements in the list or invalid index, do nothing
+        }
+
+        if (index == 0)
+        {
+            myLList.RemoveFirst(); // Remove the first node if index is 0
+            return;
+        }
+
+        LinkedListNode<int> previousNode = myLList.First;
+        LinkedListNode<int>? currentNode = previousNode.Next;
+
+        // Traverse the list until the desired index or the end of the list is reached
+        for (int i = 1; i < index && currentNode != null; i++)
+        {
+            previousNode = currentNode;
+            currentNode = currentNode.Next;
+        }
+
+        // If the current node is null, index is out of range
+        if (currentNode == null)
+        {
+            return;
+        }
+
+        // Remove the node at the given index
+        myLList.Remove(currentNode);
+    }
+}
