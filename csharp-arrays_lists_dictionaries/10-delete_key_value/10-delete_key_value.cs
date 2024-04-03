5efda0fd9633122d7c5ba16256bac6b7c44e4d89
eseosapku <75255976+eseosapku@users.noreply.github.com>
2024-04-03 09:08:32 +0200

    o

---
+++ csharp-arrays_lists_dictionaries/10-delete_key_value/10-delete_key_value.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Dictionary<string, string> myDict = new Dictionary<string, string>();
+        Dictionary<string, string> newDict = new Dictionary<string, string>();
+
+        myDict.Add("language", "C");
+        myDict.Add("track", "low level");
+        myDict.Add("school", "Holberton");
+        myDict.Add("address", "972 Mission St.");
+        myDict.Add("city", "San Francisco");
+
+        foreach (KeyValuePair<string, string> entry in myDict)
+            Console.WriteLine("{0}: {1}", entry.Key, entry.Value);
+
+        Console.WriteLine("----------------");
+
+        newDict = Dictionary.DeleteKeyValue(myDict, "track");
+
+        foreach (KeyValuePair<string, string> entry in newDict)
+            Console.WriteLine("{0}: {1}", entry.Key, entry.Value);
+
+        Console.WriteLine("-----");
+
+        foreach (KeyValuePair<string, string> entry in myDict)
+            Console.WriteLine("{0}: {1}", entry.Key, entry.Value);
+    }
+}
+
+public class Dictionary
+{
+    public static Dictionary<string, string> DeleteKeyValue(Dictionary<string, string> myDict, string key)
+    {
+        // Check if the key exists in the dictionary
+        if (myDict.ContainsKey(key))
+        {
+            // If the key exists, remove the key-value pair
+            myDict.Remove(key);
+        }
+
+        // Return the updated dictionary
+        return myDict;
+    }
+
