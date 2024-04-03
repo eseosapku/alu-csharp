6dea40b21feabb87ab486c068fd7558c2dcd34ea
eseosapku <75255976+eseosapku@users.noreply.github.com>
2024-04-03 19:52:20 +0200

    jjj

---
+++ csharp-hashset_stack_queue_linkedlist/9-linkedlist_pop/9-linkedlist_pop.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int Pop(LinkedList<int> myLList)
+    {
+        if (myLList.First == null)
+        {
+            // If the list is empty, return 0
+            return 0;
+        }
+
+        // Get the value of the head node
+        int value = myLList.First.Value;
+
+        // Remove the head node
+        myLList.RemoveFirst();
+
+        return value;
+    }
+}
