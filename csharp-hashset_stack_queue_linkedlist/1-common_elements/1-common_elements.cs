220ff4b874dd42ef0fc40806c2cc442ef7270854
eseosapku <75255976+eseosapku@users.noreply.github.com>
2024-04-03 13:10:01 +0200

    op

--- csharp-hashset_stack_queue_linkedlist/1-common_elements/1-common_elements.cs
+++ csharp-hashset_stack_queue_linkedlist/1-common_elements/1-common_elements.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+
+public class List
+{
+    public static List<int> CommonElements(List<int> list1, List<int> list2)
+    {
+        // Sort both lists
+        list1.Sort();
+        list2.Sort();
+
+        List<int> commonElements = new List<int>();
+
+        int i = 0;
+        int j = 0;
+
+        // Traverse both lists
+        while (i < list1.Count && j < list2.Count)
+        {
+            if (list1[i] == list2[j])
+            {
+                commonElements.Add(list1[i]);
+                i++;
+                j++;
+            }
+            else if (list1[i] < list2[j])
+            {
+                i++;
+            }
+            else
+            {
+                j++;
+            }
+        }
+
+        return commonElements;
+    }
+}
