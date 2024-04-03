a1e81c992fb69c63ec0e30e3e5665c9063790cf2
eseosapku <75255976+eseosapku@users.noreply.github.com>
2024-04-03 20:03:28 +0200

    ddds

--- csharp-hashset_stack_queue_linkedlist/12-linkedlist_insert/12-linkedlist_insert.cs
+++ csharp-hashset_stack_queue_linkedlist/12-linkedlist_insert/12-linkedlist_insert.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static LinkedListNode<int> Insert(LinkedList<int> myLList, int n)
+    {
+        LinkedListNode<int> newNode = new LinkedListNode<int>(n);
+
+        if (myLList.First == null || n <= myLList.First.Value)
+        {
+            myLList.AddFirst(newNode);
+            return newNode;
+        }
+
+        LinkedListNode<int> current = myLList.First;
+
+        while (current.Next != null && current.Next.Value < n)
+        {
+            current = current.Next;
+        }
+
+        // Insert the new node after the current node
+        myLList.AddAfter(current, newNode);
+
+        return newNode;
+    }
+}
