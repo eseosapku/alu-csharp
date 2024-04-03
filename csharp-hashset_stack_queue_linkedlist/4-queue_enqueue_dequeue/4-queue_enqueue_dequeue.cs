678cee86ce0082607d72d7c4dcdea9369e482fef
eseosapku <75255976+eseosapku@users.noreply.github.com>
2024-04-03 18:29:14 +0200

    com

--- csharp-hashset_stack_queue_linkedlist/4-queue_enqueue_dequeue/4-queue_enqueue_dequeue.cs
+++ csharp-hashset_stack_queue_linkedlist/4-queue_enqueue_dequeue/4-queue_enqueue_dequeue.cs
@@ -17,28 +17,18 @@ class MyQueue
         aQueue.Enqueue(newItem);
 
         bool containsSearch = false;
-        Queue<string> tempQueue = new Queue<string>();
-
-        // Process each item in the original queue
-        while (aQueue.Count > 0)
+        int count = aQueue.Count;
+        for (int i = 0; i < count; i++)
         {
-            string currentItem = aQueue.Dequeue();
-
-            // Check if the current item matches the search item
-            if (currentItem == search)
+            string item = aQueue.Dequeue();
+            if (containsSearch || item != search)
+            {
+                aQueue.Enqueue(item);
+            }
+            else
             {
                 containsSearch = true;
-                break; // Stop processing further
             }
-
-            // Otherwise, enqueue the item to the temporary queue
-            tempQueue.Enqueue(currentItem);
-        }
-
-        // Enqueue the remaining items back to the original queue
-        while (tempQueue.Count > 0)
-        {
-            aQueue.Enqueue(tempQueue.Dequeue());
         }
 
         Console.WriteLine($"Queue contains \"{search}\": {containsSearch}");
