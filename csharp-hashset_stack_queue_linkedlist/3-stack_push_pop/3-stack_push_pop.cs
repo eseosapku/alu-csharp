82ddf31c59790b5ecb7752d015dc39ce0fb11d34
eseosapku <75255976+eseosapku@users.noreply.github.com>
2024-04-03 13:55:00 +0200

    g

--- csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
+++ csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
@@ -1,49 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
 
-public static class MyStack
+class MyStack
 {
-    public static void Info(Stack<string> aStack, string newItem, string search)
+    public static Stack<string> Info(Stack<string> aStack, string newItem, string search)
     {
         Console.WriteLine($"Number of items: {aStack.Count}");
-
-        string topItem = null;
-        if (aStack.Count > 0)
-        {
-            topItem = aStack.Peek();
-            Console.WriteLine($"Top item: {topItem}");
-        }
-        else
+        
+        if(aStack.Count == 0)
         {
             Console.WriteLine("Stack is empty");
         }
-
-        Console.WriteLine($"Stack contains \"{search}\": {aStack.Contains(search)}");
-
-        if (aStack.Contains(search))
+        else
         {
-            Stack<string> tempStack = new Stack<string>();
-
-            bool found = false;
-            while (aStack.Count > 0)
-            {
-                string item = aStack.Pop();
-                if (found || item == search)
-                {
-                    found = true;
-                }
-                else
-                {
-                    tempStack.Push(item);
-                }
-            }
-
-            while (tempStack.Count > 0)
-            {
-                aStack.Push(tempStack.Pop());
-            }
+            Console.WriteLine($"Top item:{aStack.Peek()}");
         }
+        bool containsSearch = aStack.Contains(search);
+        Console.WriteLine($"Stack contains {search}: {containsSearch}");
+
+         for(int i = 0; i <= aStack.Count; i++)
+         {
+            if(containsSearch == true)
+            {
+                aStack.Pop();
+            }
+         }
 
         aStack.Push(newItem);
+
+        return aStack;
     }
 }
