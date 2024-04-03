c246f37f7a546738697abc408f649e8d4e3c2ff4
eseosapku <75255976+eseosapku@users.noreply.github.com>
2024-04-03 13:39:04 +0200

    iil

--- csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
+++ csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
@@ -18,30 +18,3 @@ public static class MyStack
         stack.Push(newItem);  // Add the new item to the top
     }
 }
-
-class Program
-{
-    static void Main(string[] args)
-    {
-        Stack<string> aStack = new Stack<string>();
-
-        aStack.Push("C");
-        aStack.Push("HTML");
-        aStack.Push("Javascript");
-        aStack.Push("Python");
-        aStack.Push("React");
-        aStack.Push("Ruby");
-
-        foreach (string item in aStack)
-            Console.WriteLine(item);
-
-        Console.WriteLine("------");
-
-        MyStack.Info(aStack, "C#", "Javascript");
-
-        Console.WriteLine("------");
-
-        foreach (string item in aStack)
-            Console.WriteLine(item);
-    }
-}
