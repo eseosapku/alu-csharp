1b42df96bfdd3960c54c89a45ca28c86b793cacc
eseosapku <75255976+eseosapku@users.noreply.github.com>
2024-04-03 19:32:48 +0200

    kk

--- csharp-hashset_stack_queue_linkedlist/6-linkedlist_length/6-linkedlist_length.cs
+++ csharp-hashset_stack_queue_linkedlist/6-linkedlist_length/6-linkedlist_length.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int Length(LinkedList<int> myLList)
+    {
+        int count = 0;
+        var currentNode = myLList.First;
+        
+        while (currentNode != null)
+        {
+            count++;
+            currentNode = currentNode.Next;
+        }
+        
+        return count;
+    }
+}
