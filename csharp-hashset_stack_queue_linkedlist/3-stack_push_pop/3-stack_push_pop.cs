255898f407c089188bba42d3eed01c032c95d893
eseosapku <75255976+eseosapku@users.noreply.github.com>
2024-04-03 13:38:21 +0200

    op

--- csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
+++ csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
@@ -1,2 +1,47 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+
+public static class MyStack
+{
+    public static void Info(Stack<string> stack, string newItem, string itemToCheck)
+    {
+        Console.WriteLine($"Number of items: {stack.Count}");
+        
+        if (stack.Count > 0)
+            Console.WriteLine($"Top item: {stack.Peek()}");
+        else
+            Console.WriteLine("Stack is empty");
+
+        Console.WriteLine($"Stack contains \"{itemToCheck}\": {stack.Contains(itemToCheck)}");
+
+        stack.Pop();  // Remove the top item
+        stack.Push(newItem);  // Add the new item to the top
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Stack<string> aStack = new Stack<string>();
+
+        aStack.Push("C");
+        aStack.Push("HTML");
+        aStack.Push("Javascript");
+        aStack.Push("Python");
+        aStack.Push("React");
+        aStack.Push("Ruby");
+
+        foreach (string item in aStack)
+            Console.WriteLine(item);
+
+        Console.WriteLine("------");
+
+        MyStack.Info(aStack, "C#", "Javascript");
+
+        Console.WriteLine("------");
+
+        foreach (string item in aStack)
+            Console.WriteLine(item);
+    }
+}
