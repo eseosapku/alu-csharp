b88330197301d8bb5393cb46d0873098f4affeca
eseosapku <75255976+eseosapku@users.noreply.github.com>
2024-04-03 18:56:50 +0200

    g

--- csharp-hashset_stack_queue_linkedlist/4-queue_enqueue_dequeue/4-queue_enqueue_dequeue.cs
+++ csharp-hashset_stack_queue_linkedlist/4-queue_enqueue_dequeue/4-queue_enqueue_dequeue.cs
@@ -3,36 +3,28 @@ using System.Collections.Generic;
 
 class MyQueue
 {
-    public static Queue<string> Info(Queue<string> aQueue, string newItem, string search)
-    {
+ public static Queue<string> Info(Queue<string> aQueue, string newItem, string search){
         Console.WriteLine($"Number of items: {aQueue.Count}");
-        
-        if (aQueue.Count == 0)
-        {
+
+        if(aQueue.Count == 0){
             Console.WriteLine("Queue is empty");
-            return aQueue;
+        }else{
+             Console.WriteLine($"First item: {aQueue.Peek()}");
         }
-        
-        Console.WriteLine($"First item: {aQueue.Peek()}");
+
         aQueue.Enqueue(newItem);
 
-        bool containsSearch = false;
-        int count = aQueue.Count;
-        for (int i = 0; i < count; i++)
-        {
-            string item = aQueue.Dequeue();
-            if (containsSearch)
-            {
-                aQueue.Enqueue(item);
-            }
-            else if (item == search)
-            {
-                containsSearch = true;
-            }
+        if(aQueue.Contains(search)){
+            Console.WriteLine($"Queue contains \"{search}\": True");
         }
 
-        Console.WriteLine($"Queue contains \"{search}\": {containsSearch}");
 
+        for(int i = 0; i <= aQueue.Count; i++){
+            if(aQueue.Contains(search)){
+                string temp = aQueue.Dequeue();
+            }
+        }
+        return aQueue;
         return aQueue;
     }
 }
