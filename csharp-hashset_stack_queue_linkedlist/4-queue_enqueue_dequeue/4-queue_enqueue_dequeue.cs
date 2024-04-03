c7fd07b3ddd4293f222d7f0bcda3dae3f0dc44ff
eseosapku <75255976+eseosapku@users.noreply.github.com>
2024-04-03 14:48:10 +0200

    g

--- csharp-hashset_stack_queue_linkedlist/4-queue_enqueue_dequeue/4-queue_enqueue_dequeue.cs
+++ csharp-hashset_stack_queue_linkedlist/4-queue_enqueue_dequeue/4-queue_enqueue_dequeue.cs
@@ -30,31 +30,3 @@ public static class MyQueue
         return aQueue;
     }
 }
-
-class Program
-{
-    static void Main(string[] args)
-    {
-        Queue<string> aQueue = new Queue<string>();
-
-        aQueue.Enqueue("C");
-        aQueue.Enqueue("HTML");
-        aQueue.Enqueue("Javascript");
-        aQueue.Enqueue("Python");
-        aQueue.Enqueue("React");
-        aQueue.Enqueue("Ruby");
-
-        foreach (string item in aQueue)
-            Console.WriteLine(item);
-
-        Console.WriteLine("------");
-
-        MyQueue.Info(aQueue, "C#", "Javascript");
-
-        Console.WriteLine("------");
-
-        foreach (string item in aQueue)
-            Console.WriteLine(item);
-    }
-}
-
