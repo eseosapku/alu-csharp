76e088fccca9bafc8cd45555381919e662b7518e
eseosapku <75255976+eseosapku@users.noreply.github.com>
2024-04-03 19:37:37 +0200

    khh

--- csharp-hashset_stack_queue_linkedlist/8-linkedlist_find/8-linkedlist_find.cs
+++ csharp-hashset_stack_queue_linkedlist/8-linkedlist_find/8-linkedlist_find.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int FindNode(LinkedList<int> myLList, int value)
+    {
+        int index = 0;
+        LinkedListNode<int> current = myLList.First;
+
+        // Iterate through the linked list
+        while (current != null)
+        {
+            // If the current node's value matches the target value, return its index
+            if (current.Value == value)
+            {
+                return index;
+            }
+
+            // Move to the next node
+            current = current.Next;
+            index++;
+        }
+
+        // If the value is not found, return -1
+        return -1;
+    }
+}
