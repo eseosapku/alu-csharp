3dc9c82f17a02765d1eaa4b81c8d097d25ea6f76
eseosapku <75255976+eseosapku@users.noreply.github.com>
2024-04-03 09:30:02 +0200

    o

---
+++ csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Dictionary
+{
+    public static string BestScore(Dictionary<string, int> myList)
+    {
+        if (myList.Count == 0)
+        {
+            return "None";
+        }
+
+        string bestKey = null;
+        int maxValue = int.MinValue;
+
+        foreach (var pair in myList)
+        {
+            if (pair.Value > maxValue)
+            {
+                maxValue = pair.Value;
+                bestKey = pair.Key;
+            }
+        }
+
+        return bestKey;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Dictionary<string, int> myDict = new Dictionary<string, int>();
+
+        myDict.Add("John", 12);
+        myDict.Add("Alex", 8);
+        myDict.Add("Bob", 14);
+        myDict.Add("Molly", 16);
+        myDict.Add("Mary", 14);
+
+        Console.WriteLine("Best Score: {0}", Dictionary.BestScore(myDict));
+    }
+}
