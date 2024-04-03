f9b858e6d4d570cd9d51a40aea89e568157a814b
eseosapku <75255976+eseosapku@users.noreply.github.com>
2024-04-03 19:39:31 +0200

    dd

---
+++ csharp-hashset_stack_queue_linkedlist/7-linkedlist_add/7-linkedlist_add.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static LinkedListNode<int> Add(LinkedList<int> myLList, int n)
+    {
+        // Create a new node with the specified value
+        LinkedListNode<int> newNode = new LinkedListNode<int>(n);
+        
+        // Insert the new node at the beginning of the linked list
+        myLList.AddFirst(newNode);
+
+        // Return the newly added node
+        return newNode;
+    }
+}
