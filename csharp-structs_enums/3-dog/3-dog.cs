36d082e4fe6d51e8cd8d16c8d299890deb27d139
eseosapku <75255976+eseosapku@users.noreply.github.com>
2024-04-04 10:06:00 +0200

    op

--- csharp-structs_enums/3-dog/3-dog.cs
+++ csharp-structs_enums/3-dog/3-dog.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public enum Rating
+{
+    Good,
+    Great,
+    Excellent
+}
+
+public struct Dog
+{
+    public string name;
+    public float age;
+    public string owner;
+    public Rating rating;
+
+    public Dog(string name, float age, string owner, Rating rating)
+    {
+        this.name = name;
+        this.age = age;
+        this.owner = owner;
+        this.rating = rating;
+    }
+
+    // Override ToString() method
+    public override string ToString()
+    {
+        return $"Dog Name: {name}\nAge: {age}\nOwner: {owner}\nRating: {rating}";
+    }
+}
