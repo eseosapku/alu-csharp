2cf97e1b323cca7393f2f0cb59c59f667885a6a1
eseosapku <75255976+eseosapku@users.noreply.github.com>
2024-04-04 09:15:16 +0200

    kk

--- csharp-exceptions/1-divide_print/1-divide_print.cs
+++ csharp-exceptions/1-divide_print/1-divide_print.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Int
+{
+    public static void divide(int a, int b)
+    {
+        int result = 0;
+        try
+        {
+            result = a / b;
+        }
+        catch (DivideByZeroException)
+        {
+            Console.WriteLine("Cannot divide by zero");
+        }
+        finally
+        {
+            Console.WriteLine($"{a} / {b} = {result}");
+        }
+    }
+}
