a9d449a4bcf126a43f5b682ce1d51e62c1c086c5
eseosapku <75255976+eseosapku@users.noreply.github.com>
2024-04-04 09:56:43 +0200

    op

--- csharp-structs_enums/0-dog/0-dog.cs
+++ csharp-structs_enums/0-dog/0-dog.cs
@@ -2,18 +2,8 @@
 
 public enum Rating
 {
-    Poor,
-    Fair,
     Good,
     VeryGood,
     Excellent
-}
-
-public struct Dog
-{
-    public string name;
-    public float age;
-    public string owner;
-    public Rating rating;
 }
 
