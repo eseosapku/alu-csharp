ef0748e761a86910fd72bf33a198804353675629
eseosapku <75255976+eseosapku@users.noreply.github.com>
2024-04-04 15:12:53 +0200

    jj

--- csharp-classes/1-enemy/1-enemy.cs
+++ csharp-classes/1-enemy/1-enemy.cs
@@ -2,30 +2,17 @@
 
 namespace Enemies
 {
-    public class Zombie
+    class Zombie
     {
-        public int health;
+        public int  health;
+        public int Health{
+            get {return health;}
+            set{health = value;}
+        }  
 
         public Zombie()
         {
-            health = 0;
-        }
-
-        public Zombie(int value)
-        {
-            if (value < 0)
-            {
-                throw new ArgumentException("Health must be greater than or equal to 0");
-            }
-            else
-            {
-                health = value;
-            }
-        }
-
-        public int GetHealth()
-        {
-            return health;
+            Health = 0;
         }
     }
 }
