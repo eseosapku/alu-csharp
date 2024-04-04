99e1836a062cb432084c1e23cd660f7cbc3afba7
eseosapku <75255976+eseosapku@users.noreply.github.com>
2024-04-04 09:43:50 +0200

    ll

--- csharp-structs_enums/2-dog/2-dog.cs
+++ csharp-structs_enums/2-dog/2-dog.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public enum Rating
+{
+    Poor,
+    Fair,
+    Good,
+    VeryGood,
+    Excellent
+}
+
+public struct Dog
+{
+    public string name;
+    public float age;
+    public string owner;
+    public Rating rating;
+
+    // Constructor with parameters
+    public Dog(string name, float age, string owner, Rating rating)
+    {
+        this.name = name;
+        this.age = age;
+        this.owner = owner;
+        this.rating = rating;
+    }
+}
+
