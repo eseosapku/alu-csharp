b3be5a7e7c6b257e0ab5841f80ca523da54e5991
eseosapku <75255976+eseosapku@users.noreply.github.com>
2024-04-04 09:16:30 +0200

    kk

--- csharp-exceptions/2-divide_lists/2-divide_lists.cs
+++ csharp-exceptions/2-divide_lists/2-divide_lists.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<int> Divide(List<int> list1, List<int> list2, int listLength)
+    {
+        List<int> divisionResults = new List<int>();
+        for (int i = 0; i < listLength; i++)
+        {
+            try
+            {
+                divisionResults.Add(list1[i] / list2[i]);
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Cannot divide by zero");
+                divisionResults.Add(0);
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                Console.WriteLine("Out of range");
+                divisionResults.Add(0);
+            }
+        }
+        return divisionResults;
+    }
+}
