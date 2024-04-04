d88730749c12a0372f137c95307f0a4183e14c8e
eseosapku <75255976+eseosapku@users.noreply.github.com>
2024-04-04 15:03:14 +0200

    jj

--- csharp-classes/2-enemy/2-enemy.cs
+++ csharp-classes/2-enemy/2-enemy.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Enemies
+{
+    public class Zombie
+    {
+        public int health;
+
+        public Zombie()
+        {
+            health = 0;
+        }
+
+        public Zombie(int value)
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Health must be greater than or equal to 0");
+            }
+            else
+            {
+                health = value;
+            }
+        }
+    }
+}
