d803a3d61d0d9054fe0220c160d608242f6fee61
eseosapku <75255976+eseosapku@users.noreply.github.com>
2024-04-04 09:39:40 +0200

    ll

---
+++ csharp-structs_enums/1-dog/1-dog.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+public enum Rating
+{
+    Poor,
+    Fair,
+    Good,
+    VeryGood,
+    Excellent
+}
+
+public struct Dog
+{
+    public string name;
+    public float age;
+    public string owner;
+    public Rating rating;
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Dog poppy;
+
+        poppy.name = "Poppy";
+        poppy.age = 0.7f;
+        poppy.owner = "Kris";
+        poppy.rating = Rating.Excellent;
+
+        Console.WriteLine("My name is {0} and I am {1}. I am an {2} dog. :)", poppy.name, poppy.age, poppy.rating);
+    }
+}
