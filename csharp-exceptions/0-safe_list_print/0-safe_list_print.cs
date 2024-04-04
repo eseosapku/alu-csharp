5f29a2933d1b0b70942357af109cde3f27bca8ea
eseosapku <75255976+eseosapku@users.noreply.github.com>
2024-04-04 09:03:16 +0200

    kk

---
+++ csharp-exceptions/0-safe_list_print/0-safe_list_print.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static int SafePrint(List<int> myList, int n)
+    {
+        int count = 0;
+        try
+        {
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine(myList[i]);
+                count++;
+            }
+        }
+        catch (ArgumentOutOfRangeException)
+        {
+            // Catching exception if n is beyond the list's range,
+            // but nothing needs to be done here as count already holds the number of elements printed
+        }
+        return count;
+    }
+}
