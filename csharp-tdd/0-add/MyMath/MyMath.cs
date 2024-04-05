eecc254f4861daf79a1a01c30ad79606f2c4a87c
eseosapku <75255976+eseosapku@users.noreply.github.com>
2024-04-05 17:01:42 +0200

    ff

---
+++ csharp-tdd/0-add/MyMath/MyMath.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using MyMath;
+
+namespace MyMath.Tests
+{
+    [TestFixture]
+    public class OperationsTests
+    {
+        [Test]
+        public void Add_WhenCalled_ReturnsSumOfTwoNumbers()
+        {
+            // Arrange
+            int a = 5;
+            int b = 7;
+
+            // Act
+            int result = Operations.Add(a, b);
+
+            // Assert
+            Assert.AreEqual(12, result);
+        }
+    }
+}
