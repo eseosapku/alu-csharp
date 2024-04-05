36f78f1643fc387f688ae1ad500384c8ca1cb4a5
eseosapku <75255976+eseosapku@users.noreply.github.com>
2024-04-05 10:11:33 +0200

    kk

---
+++ csharp-tdd/5-camelcase/MyMath.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+mkdir MyMath
+cd  MyMath
+
+dotnet new console
+
+dotnet build
+
+dotnet run
+
---
+++ csharp-tdd/5-camelcase/MyMathtests.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+mkdir MyMath.Tests
+cd MyMath.Tests
+
+dotnet new console
+
+dotnet build
+
+dotnet run
+
