9902af7de9adc59baf7985b5a3c7887bb029bf1b
eseosapku <75255976+eseosapku@users.noreply.github.com>
2024-04-05 21:11:56 +0200

    j

--- csharp-inheritance/7-shape/7-shape.cs
+++ csharp-inheritance/7-shape/7-shape.cs
@@ -1,4 +1,17 @@
-﻿using System;
+﻿public class Shape
+{
+    public virtual int Area()
+    {
+        throw new NotImplementedException("Area() method requires an implementation.");
+    }
+
+    public override string ToString()
+    {
+        return base.ToString();
+    }
+}
+
+using System;
 
 public class Rectangle : Shape
 {
@@ -7,12 +20,12 @@ public class Rectangle : Shape
 
     public int Width
     {
-        get => width;
+        get { return width; }
         set
         {
             if (value < 0)
             {
-                throw new ArgumentException("Width must be greater than or equal to 0");
+                throw new ArgumentException("Width must be greater than or equal to 0.");
             }
             width = value;
         }
@@ -20,44 +33,27 @@ public class Rectangle : Shape
 
     public int Height
     {
-        get => height;
+        get { return height; }
         set
         {
             if (value < 0)
             {
-                throw new ArgumentException("Height must be greater than or equal to 0");
+                throw new ArgumentException("Height must be greater than or equal to 0.");
             }
             height = value;
         }
     }
 
-    public override int Area()
+    // Using 'new' keyword to hide the base class Area method
+    public new int Area()
     {
-        throw new NotImplementedException("Area() is not implemented");
+        return Width * Height;
     }
 
+    // Overriding the ToString() method from the base class
     public override string ToString()
     {
-        return $"[Rectangle] {Width} / {Height}";
+        return $"[Rectangle] {width} / {height}";
     }
 }
 
-public class Square : Rectangle
-{
-    private int size;
-
-    public int Size
-    {
-        get => size;
-        set
-        {
-            if (value < 0)
-            {
-                throw new ArgumentException("Size must be greater than or equal to 0");
-            }
-            size = value;
-            Width = value;
-            Height = value;
-        }
-    }
-}
