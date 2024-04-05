74d64235fdaa4d3ee23e6ea6366d58b71cc17894
eseosapku <75255976+eseosapku@users.noreply.github.com>
2024-04-05 19:50:00 +0200

    fg

--- csharp-inheritance/3-type_get/3-type_get.cs
+++ csharp-inheritance/3-type_get/3-type_get.cs
@@ -21,16 +21,3 @@ public class Obj
         }
     }
 }
-
-class Program
-{
-    static void Main(string[] args)
-    {
-        var num = 10;
-        var myList = new List<int>();
-
-        Obj.Print(num);
-        Console.WriteLine("-----------------");
-        Obj.Print(myList);
-    }
-}
