5b82a12b4eabfbf8c8ac28742512226967de3252
eseosapku <75255976+eseosapku@users.noreply.github.com>
2024-04-05 19:23:40 +0200

    fg

--- csharp-inheritance/3-type_get/3-type_get.cs
+++ csharp-inheritance/3-type_get/3-type_get.cs
@@ -1,6 +1,19 @@
-﻿using System;
-using System.Reflection;
-using System.Linq;
+﻿using System.Reflection;
+using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var num = 9;
+        var myList = new List<int>();
+
+        Obj.Print(num);
+        Console.WriteLine("-----------------");
+        Obj.Print(myList);
+    }
+}
 
 public class Obj
 {
