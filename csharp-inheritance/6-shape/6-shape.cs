96d560fc8d9c0b516b9691a9076fa163cd00dccf
eseosapku <75255976+eseosapku@users.noreply.github.com>
2024-04-05 21:02:42 +0200

    j

--- csharp-inheritance/6-shape/6-shape.cs
+++ csharp-inheritance/6-shape/6-shape.cs
@@ -1,27 +1,5 @@
 ﻿using System;
 
-class Program
-{
-    static void Main(string[] args)
-    {
-        Rectangle aRect = new Rectangle();
-        aRect.Width = 10;
-        aRect.Height = 2;
-
-        if (typeof(Rectangle).IsSubclassOf(typeof(Shape)))
-            Console.WriteLine("Rectangle is a subclass of Shape");
-        else
-            Console.WriteLine("Rectangle is NOT a subclass of Shape");
-
-        Console.WriteLine("Width: {0}", aRect.Width);
-        Console.WriteLine("Height: {0}", aRect.Height);
-
-        // Call the Area() method explicitly
-        Console.WriteLine("Area: {0}", aRect.Area());
-    }
-   }
-using System;
-
 /// <summary>
 /// Represents a basic shape.
 /// </summary>
@@ -75,14 +53,5 @@ public class Rectangle : Shape
             }
             height = value;
         }
-    }
-
-    /// <summary>
-    /// Calculates the area of the rectangle.
-    /// </summary>
-    /// <returns>The calculated area.</returns>
-    public override int Area()
-    {
-        return width * height;
     }
 }
