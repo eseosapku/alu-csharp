df0c9b92d022ed4bd5598fa5d5c80f17b33708bc
eseosapku <75255976+eseosapku@users.noreply.github.com>
2024-04-05 19:16:06 +0200

    lll

--- csharp-inheritance/6-shape/6-shape.cs
+++ csharp-inheritance/6-shape/6-shape.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class Rectangle : Shape
+{
+    private int width;
+    private int height;
+
+    public int Width
+    {
+        get => width;
+        set
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Width must be greater than or equal to 0");
+            }
+            width = value;
+        }
+    }
+
+    public int Height
+    {
+        get => height;
+        set
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Height must be greater than or equal to 0");
+            }
+            height = value;
+        }
+    }
+
+    public override int Area()
+    {
+        throw new NotImplementedException("Area() is not implemented");
+    }
+}
