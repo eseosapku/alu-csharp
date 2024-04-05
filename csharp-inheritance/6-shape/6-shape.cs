e8dcd4c4a99b03fcb84784edc154e787c622240b
eseosapku <75255976+eseosapku@users.noreply.github.com>
2024-04-05 20:58:54 +0200

    j

--- csharp-inheritance/6-shape/6-shape.cs
+++ csharp-inheritance/6-shape/6-shape.cs
@@ -1,43 +1,88 @@
 ﻿using System;
 
+class Program
+{
+    static void Main(string[] args)
+    {
+        Rectangle aRect = new Rectangle();
+        aRect.Width = 10;
+        aRect.Height = 2;
+
+        if (typeof(Rectangle).IsSubclassOf(typeof(Shape)))
+            Console.WriteLine("Rectangle is a subclass of Shape");
+        else
+            Console.WriteLine("Rectangle is NOT a subclass of Shape");
+
+        Console.WriteLine("Width: {0}", aRect.Width);
+        Console.WriteLine("Height: {0}", aRect.Height);
+
+        // Call the Area() method explicitly
+        Console.WriteLine("Area: {0}", aRect.Area());
+    }
+   }
+using System;
+
+/// <summary>
+/// Represents a basic shape.
+/// </summary>
 public class Shape
 {
-    public virtual double Area()
+    /// <summary>
+    /// Calculates the area of the shape.
+    /// </summary>
+    /// <returns>The calculated area.</returns>
+    public virtual int Area()
     {
         throw new NotImplementedException("Area() is not implemented");
     }
 }
 
+/// <summary>
+/// Represents a rectangle, a specific type of shape.
+/// </summary>
 public class Rectangle : Shape
 {
     private int width;
     private int height;
 
+    /// <summary>
+    /// Gets or sets the width of the rectangle.
+    /// </summary>
     public int Width
     {
-        get => width;
+        get { return width; }
         set
         {
             if (value < 0)
+            {
                 throw new ArgumentException("Width must be greater than or equal to 0");
+            }
             width = value;
         }
     }
 
+    /// <summary>
+    /// Gets or sets the height of the rectangle.
+    /// </summary>
     public int Height
     {
-        get => height;
+        get { return height; }
         set
         {
             if (value < 0)
+            {
                 throw new ArgumentException("Height must be greater than or equal to 0");
+            }
             height = value;
         }
     }
 
-    public override double Area()
+    /// <summary>
+    /// Calculates the area of the rectangle.
+    /// </summary>
+    /// <returns>The calculated area.</returns>
+    public override int Area()
     {
         return width * height;
     }
 }
-
--- csharp-inheritance/6-shape/obj/Debug/net7.0/6-shape.AssemblyInfo.cs
+++ csharp-inheritance/6-shape/obj/Debug/net7.0/6-shape.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("6-shape")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8155b3f2b78c5c4698b7d51b576ff0270d27ca7e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+30b911f435211b9f6e88424d84a351036b84dc79")]
 [assembly: System.Reflection.AssemblyProductAttribute("6-shape")]
 [assembly: System.Reflection.AssemblyTitleAttribute("6-shape")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
