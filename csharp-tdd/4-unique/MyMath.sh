c8c879a193065c0316415d32c9d93782c8f5938e
eseosapku <75255976+eseosapku@users.noreply.github.com>
2024-04-05 10:11:46 +0200

    kk

---
+++ csharp-tdd/4-unique/MyMath.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+mkdir MyMath.cs
+cd MyMath.cs
+
+dotnet new console
+
+dotnet build
+
+dotnet run
+
---
+++ csharp-tdd/4-unique/MyMathtests.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+mkdir MyMath.Tests.cs
+cd MyMath.Tests.cs
+
+dotnet new console
+
+dotnet build
+
+dotnet run
