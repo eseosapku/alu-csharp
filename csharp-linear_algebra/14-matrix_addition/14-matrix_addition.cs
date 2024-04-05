0c473ed4e3b489e7a92c42fa54758abfbe8ec8e0
eseosapku <75255976+eseosapku@users.noreply.github.com>
2024-04-05 22:50:40 +0200

    p

---
+++ csharp-linear_algebra/14-matrix_addition/14-matrix_addition.cs
@@ -0,0 +1,8 @@
+﻿Create a method that adds two matrices and returns the resulting matrix.
+
+Class: MatrixMath
+Prototype: public static double[,] Add(double[,] matrix1, double[,] matrix2)
+The matrices can be either both 2D or both 3D
+2D ex.: double[,] matrix = { { 1, 2 }, { 3, 4 } };
+3D ex.: double[,] matrix = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+If any matrix is not a 2D or 3D matrix, or both matrices are not the same size, return a matrix containing -1
