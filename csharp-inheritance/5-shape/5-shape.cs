2a7b7b316ca040e1c8709df60338b1bf1c6af5bd
eseosapku <75255976+eseosapku@users.noreply.github.com>
2024-04-05 19:15:41 +0200

    lll

--- csharp-inheritance/5-shape/5-shape.cs
+++ csharp-inheritance/5-shape/5-shape.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class Shape
+{
+    public virtual int Area()
+    {
+        throw new NotImplementedException("Area() is not implemented");
+    }
+}
