25195f54d83eaaa776b50d3b0be23642e89bc0bd
eseosapku <75255976+eseosapku@users.noreply.github.com>
2024-04-05 22:34:13 +0200

    p

--- csharp-linear_algebra/3-magnitude/3-magnitude.cs
+++ csharp-linear_algebra/3-magnitude/3-magnitude.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class VectorMath
+{
+    public static double Magnitude(double[] vector)
+    {
+        if (vector.Length != 2 && vector.Length != 3)
+        {
+            return -1; // Return -1 if the vector is not 2D or 3D
+        }
+
+        double sumOfSquares = 0;
+        foreach (double component in vector)
+        {
+            sumOfSquares += component * component; // Sum the squares of each component
+        }
+
+        double magnitude = Math.Sqrt(sumOfSquares); // Calculate the square root of the sum
+        return Math.Round(magnitude, 2); // Round the magnitude to the nearest hundredth
+    }
+}
