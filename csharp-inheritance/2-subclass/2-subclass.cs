4def7e2cd184f80abd868cc70ad658921081668f
eseosapku <75255976+eseosapku@users.noreply.github.com>
2024-04-05 19:09:45 +0200

    lll

--- csharp-inheritance/2-subclass/2-subclass.cs
+++ csharp-inheritance/2-subclass/2-subclass.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class Obj
+{
+    public static bool IsOnlyASubclass(Type derivedType, Type baseType)
+    {
+        return derivedType != baseType && baseType.IsAssignableFrom(derivedType);
+    }
+}
