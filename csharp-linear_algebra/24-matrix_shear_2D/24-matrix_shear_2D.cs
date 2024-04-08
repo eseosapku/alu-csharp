449c6d92c724e73d29a235289361836f65609d7e
eseosapku <75255976+eseosapku@users.noreply.github.com>
2024-04-08 15:43:43 +0200

    fd

--- csharp-linear_algebra/24-matrix_shear_2D/24-matrix_shear_2D.cs
+++ csharp-linear_algebra/24-matrix_shear_2D/24-matrix_shear_2D.cs
@@ -1,64 +1,50 @@
 ﻿using System;
 
-/// <summary>
-/// Provides methods for matrix operations.
-/// </summary>
 public class MatrixMath
 {
     /// <summary>
     /// Shears a square 2D matrix by a given shear factor and returns the resulting matrix.
     /// </summary>
     /// <param name="matrix">The square 2D matrix to shear.</param>
-    /// <param name="direction">The direction of the shear (either 'x' or 'y').</param>
+    /// <param name="direction">The direction of the shear (x or y).</param>
     /// <param name="factor">The shear factor.</param>
-    /// <returns>The resulting matrix after shearing.</returns>
-    /// <remarks>
-    /// The shear factor can be in either the X or Y direction but not both.
-    /// If the matrix is of an invalid size or if given an axis that is not x or y, 
-    /// the method returns a matrix containing -1.
-    /// </remarks>
+    /// <returns>The resulting sheared matrix or a matrix containing -1 if the input is invalid.</returns>
     public static double[,] Shear2D(double[,] matrix, char direction, double factor)
     {
-        // Get the dimensions of the matrix
-        int rows = matrix.GetLength(0);
-        int cols = matrix.GetLength(1);
-
         // Check if the matrix is square
-        if (rows != cols)
+        if (matrix.GetLength(0) != matrix.GetLength(1))
         {
-            // Return a matrix containing -1 if the matrix is not square
-            return new double[,] { { -1 } };
+            return new double[,] { { -1 } }; // Return -1 for invalid matrix size
         }
 
         // Check if the direction is valid
         if (direction != 'x' && direction != 'y')
         {
-            // Return a matrix containing -1 if the direction is invalid
-            return new double[,] { { -1 } };
+            return new double[,] { { -1 } }; // Return -1 for invalid direction
         }
 
-        // Create a new matrix to store the sheared values
-        double[,] shearedMatrix = new double[rows, cols];
+        // Shear the matrix based on the direction
+        double[,] shearedMatrix = new double[matrix.GetLength(0), matrix.GetLength(1)];
 
-        // Perform shearing for each element in the matrix
-        for (int i = 0; i < rows; i++)
+        if (direction == 'x')
         {
-            for (int j = 0; j < cols; j++)
+            // Shear along the x-axis
+            for (int i = 0; i < matrix.GetLength(0); i++)
             {
-                if (direction == 'x')
-                {
-                    // Shear along the x-axis
-                    shearedMatrix[i, j] = matrix[i, j + (int)(i * factor)];
-                }
-                else
-                {
-                    // Shear along the y-axis
-                    shearedMatrix[i, j] = matrix[i + (int)(j * factor), j];
-                }
+                shearedMatrix[i, 0] = matrix[i, 0] + (factor * matrix[i, 1]);
+                shearedMatrix[i, 1] = matrix[i, 1];
+            }
+        }
+        else if (direction == 'y')
+        {
+            // Shear along the y-axis
+            for (int j = 0; j < matrix.GetLength(1); j++)
+            {
+                shearedMatrix[0, j] = matrix[0, j];
+                shearedMatrix[1, j] = matrix[1, j] + (factor * matrix[0, j]);
             }
         }
 
         return shearedMatrix;
     }
 }
-
