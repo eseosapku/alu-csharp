2b81115255603084cde89f4f9da67231ff5df3e0
eseosapku <75255976+eseosapku@users.noreply.github.com>
2024-04-08 13:58:39 +0200

    fd

--- csharp-linear_algebra/16-matrix_scalar_mul/obj/Debug/net8.0/16-matrix_scalar_mul.AssemblyInfo.cs
+++ csharp-linear_algebra/16-matrix_scalar_mul/obj/Debug/net8.0/16-matrix_scalar_mul.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("16-matrix_scalar_mul")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+06d39efb5000148ab6db7cc59a0cee1318fe736c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+656f353f1140374fce245f4a0f571cab0dfcf62b")]
 [assembly: System.Reflection.AssemblyProductAttribute("16-matrix_scalar_mul")]
 [assembly: System.Reflection.AssemblyTitleAttribute("16-matrix_scalar_mul")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
