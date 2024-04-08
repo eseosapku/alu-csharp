c33ad71b8199d300df8ecd8f0c36f1ac38c81f2a
eseosapku <75255976+eseosapku@users.noreply.github.com>
2024-04-08 15:18:19 +0200

    fd

--- csharp-linear_algebra/30-cross_product/30-cross_product.cs
+++ csharp-linear_algebra/30-cross_product/30-cross_product.cs
@@ -23,6 +23,15 @@ public class VectorMath
         result[1] = (vector1[2] * vector2[0]) - (vector1[0] * vector2[2]);
         result[2] = (vector1[0] * vector2[1]) - (vector1[1] * vector2[0]);
 
+        // Check if result is close to zero, consider it as zero
+        for (int i = 0; i < 3; i++)
+        {
+            if (Math.Abs(result[i]) < 0.00001)
+            {
+                result[i] = 0;
+            }
+        }
+
         return result;
     }
 }
