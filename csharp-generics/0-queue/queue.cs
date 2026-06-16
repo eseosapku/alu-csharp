52012b85db772c53c7b57eacd3f6e875d48e35c9
Eseosa Kay-Uwagboe Pascal <75255976+eseosapku@users.noreply.github.com>
2026-06-16 23:23:13 +0200

    ll

--- csharp-generics/0-queue/queue.cs
+++ csharp-generics/0-queue/queue.cs
@@ -1,6 +1,15 @@
-﻿using System;
+using System;
+
+/// <summary>
+/// Represents a custom first-in, first-out generic collection.
+/// </summary>
+/// <typeparam name="T">The type of elements in the queue.</typeparam>
 public class Queue<T>
 {
+    /// <summary>
+    /// Gets the type of the generic Queue.
+    /// </summary>
+    /// <returns>The Type of the generic parameter.</returns>
     public Type CheckType()
     {
         return typeof(T);
